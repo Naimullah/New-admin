0b6eed7f0bd0f7b8f3bb5f8255e1ec09bcb8d321
naim <naimullah.1400@gmail.com>
2023-06-07 15:43:36 +0430

    registeration anf login compononet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from "./users_mmanagement/register/register.component";
 import { DashboardComponent } from './dashboard.component';
 import { AppRoutingModule } from './app.routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar.component';
 import { SidebarComponent } from './sidebar.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { GenerateComponent } from './components/forms/general/generate.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from 'src/material.module';
+import {HttpClientModule} from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    RegisterComponent,
     NavbarComponent,
     SidebarComponent,
     FormsComponent,
@@ -23,8 +29,12 @@ import { GenerateComponent } from './components/forms/general/generate.component
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,13 +4,13 @@ import { DashboardComponent } from "./dashboard.component";
 import { FormsComponent } from "./components/forms/forms.component";
 import { GenerateComponent } from "./components/forms/general/generate.component";
 import { LoginComponent } from "./users_mmanagement/login/login.component";
-import { SignupComponent } from "./users_mmanagement/signup/signup.component";
+import { RegisterComponent } from "./users_mmanagement/register/register.component";
 const routes: Routes=[
 {path: '', component:DashboardComponent},
 {path: 'dashboard', component:DashboardComponent},
 {path: 'generate', component:GenerateComponent},
 {path: 'login', component:LoginComponent},
-{path: 'signup', component:SignupComponent},
+{path: 'register', component:RegisterComponent},
 {path: 'forms', component:FormsComponent,children: [
     {path: '', component:GenerateComponent},
     {path: 'generate', component:GenerateComponent}
